354f3b46f11a5df12edb5e4bb4bef8e9343c2018
Aboli <arshiabolghasemi@gmail.com>
2026-07-25 12:16:55 +0330

    fix:: fix run portfolio command

--- scripts/run_portfolio.sh
+++ scripts/run_portfolio.sh
@@ -4,5 +4,5 @@ holdings=(1 2 3 5 10 20 40 60)
 
 for day in "${holdings[@]}"; do
     echo "Running portfolio optimization for holding period: ${day} days..."
-    uv run python -m scripts.tyche.portfolio --holding "$day"
+    uv run python -m tyche.portfolio.run --holding "$day"
 done
